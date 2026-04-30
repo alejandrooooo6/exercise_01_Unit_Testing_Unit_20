11a7c23a6afdc0d76653c15361d611af45586424
Alex <alejandroaf@msmk.university>
2026-04-30 11:11:38 +0200

    Finised coding the customer class and wrote the unit tests for it

--- exercise-01-Unit_Testing/src/customer.ts
+++ exercise-01-Unit_Testing/src/customer.ts
@@ -1,19 +1,38 @@
-/**
- * Represents a customer with specific tax and discount rules.
- */
+import { AbstractCustomer } from "./abstract.customer.js";
 
- import { AbstractCustomer } from "./abstract.customer.js";
+export class Customer extends AbstractCustomer {
+    private name: string;
+    private type: 'regular' | 'vip' | 'business';
 
- export class Customer extends AbstractCustomer{
-   private name: string;
-    //// 
-    //// 
-    //// FILL HERE THE CODE OF Customer 
-    //// 
-    //// 
+    constructor(name: string, type: 'regular' | 'vip' | 'business') {
+        super(name, type);
+        this.name = name;
+        this.type = type;
+    }
+
+    public getDiscountRate(): number {
+        if (this.type === 'vip') {
+            return 0.50; 
+        } else if (this.type === 'regular') {
+            return 0.15;
+        } else if (this.type === 'business') {
+            return 0;    
+        } else {
+            return 0;  
+        }
+    }
+
+    public getTaxRate(): number {
+        if (this.type === 'business') {
+            return 0.10; 
+        } else if (this.type === 'regular' || this.type === 'vip') {
+            return 0.21; 
+        } else {
+            return 0.21; 
+        }
+    }
 
     public getName(): string {
-      return this.name;
+        return this.name;
     }
- }
- 
+}
